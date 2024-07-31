508e3dedcf675fb1259f7e49fd7c3b93c68da5d6
Michael Byrd <iamtron01@hotmail.com>
2024-07-31 15:16:46 -0500

    fixed typo

--- sql-injection/sqli-lab-02.py
+++ sql-injection/sqli-lab-02.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
             print("[-] SQL injection unsuccessful")
     except IndexError:
         print("[-] Usage: %s <url> <user>" % sys.argv[0])
-        print('[-] Example: %s www.example.com "1=1"' % sys.arv[0])
+        print('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
         sys.exit(FAIL)
     except Exception as exception:
         print("An exception occured %s" % exception)
